c635ff8074b47a67fb8a4b75dd4f00589e2ec348
andresmc <andriu_012@hotmail.com>
2018-06-02 16:39:23 -0500

    Se cambia el tcp

--- serverSocket/index.js
+++ serverSocket/index.js
@@ -11,29 +11,8 @@ server.on('connection', function (socket) {
     });
  
     var a = {item: 'A', a: 'asda'};
-    var b = {item: 'B', b: 'asdasa'};
-    var c = {item: 'asdadsadsadsasd87a8ds7ads67ad6sa6dsa', a: 1, b: 2, c: 3, d: 'asda', d: 'asdadsa'};
-    var d = '{"item": "asdaasda", "a": "1", "b": "2", "c": "3", "d": "asda", "d": "asdasda"}';
-    var e = '{"item": "ee", "a": "1", "d": "643", "d": "123"}';
-    var f = '{"item": "dd", "a": "1", "d": "643", "d": "123"}';
  
     socket.write(a);
-    socket.write(a);
-    socket.write(a);
-    socket.write(b);
- 
-    setInterval(function () {
-        socket.write(e);
-    }, 1000);
- 
-    setInterval(function () {
-        socket.write(f);
-    }, 1500);
- 
-    socket.write(d);
-    socket.write(c);
-    socket.write(b);
-    socket.write(b);
 });
  
 server.listen(5055, '0.0.0.0');
