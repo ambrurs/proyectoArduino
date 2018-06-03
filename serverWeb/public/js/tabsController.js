b6b565bd9cf0c9bad14da679a249d1fd850c1d90
andresmc <andriu_012@hotmail.com>
2018-06-03 16:50:16 -0500

    Se agrega el tab de la fotocelda

--- serverWeb/public/js/tabsController.js
+++ serverWeb/public/js/tabsController.js
@@ -2,9 +2,9 @@
   factory('mySocket', function (socketFactory) {
     var Isocket = io('http://localhost:8081');
     mySocket = socketFactory({
-        ioSocket: Isocket
-      });
-      return socketFactory();
+      ioSocket: Isocket
+    });
+    return socketFactory();
   }).
   controller('UserManagementController', function($scope,mySocket,$http){
 
@@ -14,27 +14,27 @@
     //LEDS 
 
     $scope.encenderAmarillo = function(){
-          mySocket.emit('ledColor','#FFE400');
+      mySocket.emit('ledColor','#FFE400');
     }
 
     $scope.encenderRojo = function(){
-          mySocket.emit('ledColor','#FF0000');
+      mySocket.emit('ledColor','#FF0000');
     }
 
     $scope.encenderAzul = function(){
-          mySocket.emit('ledColor','#0000FF');
+      mySocket.emit('ledColor','#0000FF');
     }
 
     $scope.encenderVerde = function(){
-          mySocket.emit('ledColor','#00FF00');
+      mySocket.emit('ledColor','#00FF00');
     }
 
     $scope.encenderMorado = function(){
-          mySocket.emit('ledColor','#9A2EFE');
+      mySocket.emit('ledColor','#9A2EFE');
     }
 
     $scope.colorModal = function(){
-        document.getElementById('color').click();
+      document.getElementById('color').click();
     }  
 
     $scope.colorSend = function(){
@@ -54,10 +54,10 @@
 
     function hexToRgb(hex) {
       // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
         return r + r + g + g + b + b;
-    });
+      });
 
       var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
       return result ? {
@@ -66,17 +66,18 @@
         b: parseInt(result[3], 16)
       } : null;
     }
-   
-   $scope.onServo = function(){
-    document.getElementById("gear").classList.remove('gear-innerS');
-    document.getElementById("gear").classList.add('gear-inner');
-    
-   }
 
-  $scope.offServo = function(){
-    document.getElementById("gear").classList.remove('gear-inner');
-    document.getElementById("gear").classList.add('gear-innerS');
-   }
+    $scope.onServo = function(){
+      document.getElementById("gear").classList.remove('gear-innerS');
+      document.getElementById("gear").classList.add('gear-inner');
+
+    }
+
+
+    $scope.offServo = function(){
+      document.getElementById("gear").classList.remove('gear-inner');
+      document.getElementById("gear").classList.add('gear-innerS');
+    }
 
     mySocket.on('temperaturaHtml',function(data){
       $scope.temp = data;
@@ -93,6 +94,19 @@
         document.getElementById("h1Temp").classList.remove('temperatura-color2');
         document.getElementById("h1Temp").classList.remove('temperatura-color3');
         document.getElementById("h1Temp").classList.add('temperatura-color');
+      }
+    });
+
+    mySocket.on('luz',function(data){
+      
+      if (data) {
+        $("#fullpage").removeClass("night");
+        $("#switch").removeClass("switched");
+      }
+      else {
+        $("#fullpage").addClass("night");
+        $("#switch").addClass("switched");
+
       }
     });
 
