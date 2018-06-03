23bb85580bcc281efbcddd3fd6b06ff4e8080ff4
andresmc <andriu_012@hotmail.com>
2018-06-03 17:53:11 -0500

    Se agrea validacion

--- serverWeb/public/js/tabsController.js
+++ serverWeb/public/js/tabsController.js
@@ -98,7 +98,7 @@
     });
 
     mySocket.on('luz',function(data){
-
+      console.log(data);
       if (data === 'true') {
         $("#fullpage").removeClass("night");
         $("#switch").removeClass("switched");
